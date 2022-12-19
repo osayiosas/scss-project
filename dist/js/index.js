a5e3bd7bbeaabbb3a6700fe91e67402c1590f8d8
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2022-12-19 14:10:58 +0100

    sass project

---
+++ dist/js/index.js
@@ -0,0 +1,6 @@
+//select DOM items
+
+const meanBtn = document.querySelector('.btn-menu');
+const mean = document.querySelector('.menu');
+const meanNav = document.querySelector('.menu-nav');
+const meanBranding = document.querySelector('.menu-branding')
